08a18eda4ffd4b69a605baaadc84c2db74a94c4d
Armotea <gong322@mail.ru>
2023-10-14 20:23:36 +0300

    create two vectors

---
+++ vectors.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	vector<int> data = { 1, 2, 3, 4, 5, 6 };
+	for (int item : data) {
+		cout << item << "\n";
+	}
+	cout << "--------------------";
+	vector<string> stroke = { "meow", "cute", "ooops", "hello" };
+	for (int i = 0; i < stroke.size(); ++i) {
+		cout << stroke[i] << "\n";
+	}
+}
